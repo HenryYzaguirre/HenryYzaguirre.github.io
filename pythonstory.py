82b6e1c1ca88fedcb722ea3213ecbdcd92e446af
HenryYzaguirre <31449531+HenryYzaguirre@users.noreply.github.com>
2017-12-13 08:22:59 -0800

    Add files via upload

---
+++ pythonstory.py
@@ -0,0 +1,50 @@
+#My python story
+
+def greeting():
+    print("Hello my name is gig bigger....")
+    response=input('Do you want to play a game you gig bigger?(yes/no)')
+    return response
+
+def second_choice():
+     print("Great....now eat my hamburgers")
+     response=input('Do you eat them?(yes/no/probably)...')
+     return response
+
+def third_choice():
+    print('do you want to eat my curly fries instead?...')
+    response=input('do you just want to eat them?(yes/no/)...')
+    return response
+
+def fourth_choice():
+    print('So I guess you already had dinner you GIG BIGGER?!...')
+    response=input('(yes/no)')
+    return response
+
+                
+    
+
+def haters():
+    print("Ok, you suck")
+def opened():
+    print("Ok giggie")
+
+def not_opened():
+        print("you live")
+
+
+x = greeting()
+y = second_choice()
+z = third_choice()
+if x=="no": 
+   y = second_choice()
+if y=="yes":
+    
+    if z=="probably":
+        other()
+
+
+
+
+
+    
+
